8d0c42b5e5bd91f31567dbf14918cfc305b90e27
Adam Loo <apl5197@psu.edu>
2018-09-05 16:43:35 -0400

    added gaussin smoothing script and example output

---
+++ smoothImageWithGaussin.m
@@ -0,0 +1,47 @@
+%Initially read in file
+%and convert to a greyscale image
+%by averageing the RGB pixle values
+im = imread('dma.jpg');
+im = double(im);
+grayIm = (im(:,:,1) + im(:,:,2) + im(:,:,3)) / 3;
+
+
+%right now just display
+figure(1);
+colormap(gray);
+imagesc(grayIm);
+
+%save intermediat file
+imwrite(uint8(grayIm),'grayScaleIntermediate.jpg','jpeg');
+
+%generate 2d gaussin filter
+%   play with sigma values
+%   the xArray goes from -(3*sigma):(3*sigma)
+%   so need to change too when testing
+sigma = 8;
+xArray = -24:24;
+
+%first make gaussin 1D
+gaussin1D = exp(-1/(2*sigma^2) * xArray.^2); %not entirely sure what this is
+gaussin1D = gaussin1D / sum(gaussin1D);
+
+%then filter against itself to make 2D
+gaussin2D = imfilter(gaussin1D,gaussin1D','full');
+figure(2);
+imagesc(gaussin2D);
+
+%oneSmooth = imfilter(imfilter(im,gaussin1D','same'),gaussin1D,'same');
+
+%filter the image with this new 2D filter
+twoSmooth = imfilter(im,gaussin2D,'same','replicate');
+graySmooth = (twoSmooth(:,:,1) + twoSmooth(:,:,2) + twoSmooth(:,:,3)) / 3;
+
+figure(3);
+colormap(gray);
+imagesc(graySmooth);
+
+%print out smooth image
+imwrite(uint8(graySmooth),'smoothedImage.jpg','jpeg');
+
+
+
