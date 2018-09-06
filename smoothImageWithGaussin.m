73521da4db7e8db918d1731f8580b27279459077
Alec <alecshmitz22@gmail.com>
2018-09-06 18:42:59 -0400

    adding template clipping

--- smoothImageWithGaussin.m
+++ smoothImageWithGaussin.m
@@ -7,9 +7,9 @@ grayIm = (im(:,:,1) + im(:,:,2) + im(:,:,3)) / 3;
 
 
 %right now just display
-figure(1);
+%figure(1);
 colormap(gray);
-imagesc(grayIm);
+%imagesc(grayIm);
 
 %save intermediat file
 imwrite(uint8(grayIm),'grayScaleIntermediate.jpg','jpeg');
@@ -27,8 +27,8 @@ gaussin1D = gaussin1D / sum(gaussin1D);
 
 %then filter against itself to make 2D
 gaussin2D = imfilter(gaussin1D,gaussin1D','full');
-figure(2);
-imagesc(gaussin2D);
+%figure(2);
+%imagesc(gaussin2D);
 
 %oneSmooth = imfilter(imfilter(im,gaussin1D','same'),gaussin1D,'same');
 
@@ -36,12 +36,28 @@ imagesc(gaussin2D);
 twoSmooth = imfilter(im,gaussin2D,'same','replicate');
 graySmooth = (twoSmooth(:,:,1) + twoSmooth(:,:,2) + twoSmooth(:,:,3)) / 3;
 
-figure(3);
+%figure(3);
 colormap(gray);
-imagesc(graySmooth);
+%imagesc(graySmooth);
 
 %print out smooth image
 imwrite(uint8(graySmooth),'smoothedImage.jpg','jpeg');
 
+fprintf(1,'Select template');
+figure(1);
+colormap(gray);
+[x,y] = ginput(1);
+x = round(x); y = round(y);
+hold on;
+plot(x,y,'r*');
+halfwid = 30;
+plot([x-halfwid x-halfwid x+halfwid x+halfwid],...
+     [y-halfwid y+halfwid y+halfwid y-halfwid],'r');
+hold off; drawnow;
 
+deltavac = -halfwid:halfwid;
+patch = double(graySmooth(y+deltavac, x+deltavac));
 
+imwrite(uint8(patch),'template.jpg','jpeg');
+figure(2);
+imagesc(patch);
